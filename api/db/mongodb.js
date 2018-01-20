3fa04e60fe7509dc1ecf549f7a90390c13d2a429
Varvara Zadnepriak <Varvara_Zadnepriak@epam.com>
2018-01-20 22:41:42 +0400

    Switch DB to dev

--- api/db/mongodb.js
+++ api/db/mongodb.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const dbuser = 'vote-app';
 const dbpassword = 'vote-app-1';
 
+const connectionString = process.env.CONNECTION_STR ||
+    `mongodb://${dbuser}:${dbpassword}@ds211088.mlab.com:11088/vote-app-dev`;
+
 module.exports = {
     connect
 };
 
 function connect() {
-    return mongoose.connect(`mongodb://${dbuser}:${dbpassword}@ds239137.mlab.com:39137/vote-app`);
+    return mongoose.connect(connectionString);
 }
