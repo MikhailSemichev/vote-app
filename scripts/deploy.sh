e34b75e3b34ac453ef910cc40a29293133adc858
Varvara Zadnepriak <Varvara_Zadnepriak@epam.com>
2019-01-13 20:36:02 +0400

    Remove shell script, inline deploy command

--- scripts/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# This is run by circle.yml script as part of continuous integration
-# to run manually, pass in a --version=$SERVICE_VERSION argument
-
-    # deploy to AppEngine
-    # where $1 = --version=$SERVICE_VERSION
-    gcloud -q app deploy app.yaml --promote --version=1
