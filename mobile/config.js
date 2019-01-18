f977ae0196c06d06994d97afff77a57378e0565e
VarvaraZadnepriak <Varvara_Zadnepriak@epam.com>
2019-01-18 15:53:10 +0400

    Test commit

--- mobile/config.js
+++ mobile/config.js
@@ -1,5 +1,5 @@
 export default {
     SERVICE_URL: process.env.SERVICE_URL ?
         process.env.SERVICE_URL.trim() // PRODUCTION MODE
-        : 'https://va0.herokuapp.com' // DEVELOPMENT MODE
+        : 'http://vote-app-prod.eu-central-1.elasticbeanstalk.com/' // DEVELOPMENT MODE
 };
--- web/src/config.js
+++ web/src/config.js
@@ -1,5 +1,5 @@
 export default {
     SERVICE_URL: process.env.SERVICE_URL ?
-        process.env.SERVICE_URL.trim() // PRODUCTION MODE
+        'http://vote-app-prod.eu-central-1.elasticbeanstalk.com/' // PRODUCTION MODE
         : 'http://localhost:3333' // DEVELOPMENT MODE
 };
